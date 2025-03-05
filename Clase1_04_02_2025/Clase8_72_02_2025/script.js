3b20fe382e2cf5bcad903832156b205c5125aea4
adolfo <aldomc99@gmail.com>
2025-03-05 11:23:55 -0600

    se modifica script.js

--- Clase1_04_02_2025/Clase8_72_02_2025/script.js
+++ Clase1_04_02_2025/Clase8_72_02_2025/script.js
@@ -2,6 +2,7 @@ const formulario = document.getElementById("coment-form")
 const listaComentarios = document.getElementById("dynamicList")
 const btnborra = document.getElementById("borrarcoment") 
 
+
 class Comentarios {
     constructor(texto){
         this.texto = texto
@@ -11,20 +12,21 @@ class Comentarios {
     }
 }
 
-
 formulario.addEventListener("submit", function(event){
     event.preventDefault()
   console.log(event);
 
-  const text = document.getElementById("itemComentario").value
+  const text = document.getElementById("itemComentario").value.trim();
+  if (text === "") return;
   const item = document.createElement("li")
 
   let nuevoComentario = new Comentarios(text)
 
   item.textContent = nuevoComentario.mostrarComentario()
   listaComentarios.appendChild(item)
-  
+
   formulario.reset()
+
 })
 
 btnborra.addEventListener("click", eliminarComentario);
@@ -34,8 +36,16 @@ function eliminarComentario() {
         listaComentarios.removeChild(ultimoComentario)
     } else {
         alert("No hay comentarios que eliminar")
-    }
+    };
+    
 }
+
+/*function mostrarFecha(){
+    let date = new Date ();
+    let datos = {day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit" };
+    return date.toLocaleDateString("es-ES", datos);
+}
+console.log(mostrarFecha);*/ 
 
 /*
 localStorage.setItem("nombre", "carlos")
